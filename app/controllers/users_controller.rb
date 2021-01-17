c00bea99c14b2ca6d3c1e023da8278c97baf96ac
Venkata Subhash <venkata.subhash@scripbox.com>
2021-01-17 17:42:55 +0530

    Fix User Availability issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,13 @@ class UsersController < ApplicationController
   def availability
     @users_to_select = User.all.map(&:user_name)
     if availability_params[:availability]
-      status =
-          CalendarEvents::Services::UserAvailability.new(availability_params[:availability]).status
-      @user_availability_details = availability_params[:availability].merge!(status: status)
+      service = CalendarEvents::Services::UserAvailability.new(availability_params[:availability])
+      @user_availability_details = {
+        user_name: service.user_name,
+        start_time: service.start_time,
+        end_time: service.end_time,
+        status: service.status
+      }
     end
   end
 
--- lib/calendar_events/services/user_availability.rb
+++ lib/calendar_events/services/user_availability.rb
@@ -3,15 +3,16 @@
 module CalendarEvents
   module Services
     class UserAvailability
-      attr_reader :user_name, :start_time, :end_time
+      attr_reader :user_name, :opts, :start_time, :end_time
 
       AVAILABLE_STATUS = 'Available'
       NOT_AVAILABLE_STATUS = 'Not Available'
 
       def initialize(opts = {})
         @user_name = opts[:user_name]
+        @opts = opts
         @start_time = opts[:start_time]
-        @end_time = opts[:end_time] || @start_time + 2.hours
+        @end_time = calculate_end_time
       end
 
       def status
@@ -22,6 +23,14 @@ module CalendarEvents
 
       def user
         @user ||= User.where(user_name: user_name).last
+      end
+
+      def calculate_end_time
+        if opts[:end_time] == ''
+          Time.zone.parse(opts[:start_time]) + 2.hours
+        else
+          opts[:end_time]
+        end
       end
     end
   end
